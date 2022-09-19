e396c2936cee7be523423309c050acc3be51bf81
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2022-09-19 16:16:59 +0900

    ブロックチェーンの整合性チェック

--- BlockChain.cpp
+++ BlockChain.cpp
@@ -3,74 +3,101 @@
 #include <vector>
 
 class Transaction {
-  public:
-    Transaction(std::string sender, std::string receiver, double amount) {
-      this->sender = sender;
-      this->receiver = receiver;
-      this->amount = amount;
-    }
+ public:
+  Transaction(std::string sender, std::string receiver, double amount) {
+    this->sender = sender;
+    this->receiver = receiver;
+    this->amount = amount;
+  }
 
-    std::string toString() {
-      return sender + " -> " + receiver + " : $" + std::to_string(amount);
-    }
+  std::string toString() {
+    return sender + " -> " + receiver + " : $" + std::to_string(amount);
+  }
 
-  private:
-    std::string sender;
-    std::string receiver;
-    double amount;
+ private:
+  std::string sender;
+  std::string receiver;
+  double amount;
 };
 
 class Block {
-  public:
-    Block(size_t previousHash, Transaction* transaction) {
-      this->previousHash = previousHash;
-      this->transaction = transaction;
-      this->timestamp = time(nullptr);
-      this->hash = calcHash();
-    }
+ public:
+  Block(size_t previousHash, Transaction* transaction) {
+    this->previousHash = previousHash;
+    this->transaction = transaction;
+    this->timestamp = time(nullptr);
+    this->hash = calcHash();
+  }
 
-    void printInfo() {
-      std::cout << "hash:\t\t" << hash << "\nprevious hash:\t" << previousHash << "\ntransaction:\t" << transaction->toString() << "\ntimestamp:\t" << std::string(ctime(&timestamp)) << std::endl;
-    }
+  void printInfo() {
+    std::cout << "hash:\t\t" << hash << "\nprevious hash:\t" << previousHash
+              << "\ntransaction:\t" << transaction->toString()
+              << "\ntimestamp:\t" << std::string(ctime(&timestamp))
+              << std::endl;
+  }
 
-    size_t getHash() {
-      return hash;
-    }
+  size_t calcHash() {
+    std::stringstream ss;
+    ss << previousHash << timestamp << transaction->toString();
+    return std::hash<std::string>{}(ss.str());
+  }
 
-  private:
-    size_t previousHash;
-    size_t hash;
-    time_t timestamp;
-    Transaction* transaction;
+  size_t getHash() { return hash; }
 
-    size_t calcHash() {
-      std::stringstream ss;
-      ss << previousHash << timestamp << transaction->toString();
-      return std::hash<std::string>{}(ss.str());
-    }
+  size_t getPreviousHash() { return previousHash; }
+
+ private:
+  size_t previousHash;
+  size_t hash;
+  time_t timestamp;
+  Transaction* transaction;
 };
 
 class BlockChain {
-  public:
-    BlockChain() {
-      Block* genesis = new Block(0, new Transaction("genesis", "genesis", 0));
-      chain.push_back(genesis);
-    }
+ public:
+  BlockChain() {
+    Block* genesis = new Block(0, new Transaction("genesis", "genesis", 0));
+    chain.push_back(genesis);
+  }
 
-    void addBlock(Transaction* transaction) {
-      Block* block = new Block(chain.back()->getHash(), transaction);
-      chain.push_back(block);
-    }
+  void addBlock(Transaction* transaction) {
+    Block* block = new Block(chain.back()->getHash(), transaction);
+    chain.push_back(block);
+  }
 
-    void printInfo() {
-      for (auto block : chain) {
-        std::cout << "--------------------------------------------------------------------------" << std::endl;
-        block->printInfo();
+  void addTransaction(Transaction* transaction) {
+    pendings.push_back(transaction);
+  }
+
+  bool isValid() {
+    for (size_t i = chain.size() - 1; i > 0; --i) {
+      Block* curBlock = chain[i];
+      Block* preBlock = chain[i - 1];
+
+      if (curBlock->getHash() != curBlock->calcHash()) {
+        return false;
+      }
+
+      if (curBlock->getPreviousHash() != preBlock->getHash()) {
+        return false;
       }
     }
-  
-  private:
-    std::vector<Block*> chain;
+
+    return true;
+  }
+
+  void printInfo() {
+    for (auto block : chain) {
+      std::cout << "-----------------------------------------------------------"
+                   "---------------"
+                << std::endl;
+      block->printInfo();
+    }
+  }
+
+ private:
+  std::vector<Block*> chain;
+  std::vector<Transaction*> pendings;
 };
 
 int main(void) {
@@ -78,6 +105,6 @@ int main(void) {
   bc->addBlock(new Transaction("hoge", "huga", 100));
   bc->addBlock(new Transaction("hoge", "huga", 5000));
   bc->printInfo();
-  
+
   return 0;
 }
